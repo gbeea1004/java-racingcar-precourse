36efb60be06cd7415bf2170e4844670566013372
my <jvckigh@gmail.com>
2022-10-10 00:40:56 +0900

    test: 자동차 전진 조건 테스트 추가
    - random 값이 4 이상일 경우 전진, 3 이하일 경우 멈춘다.

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,13 +1,28 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
 
 class CarTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("test");
+    }
 
     @DisplayName("자동차 이름은 5자 이하만 가능하다")
     @ParameterizedTest
@@ -30,5 +45,41 @@ class CarTest {
         // then
         assertThatThrownBy(() -> new Car(name)).isInstanceOf(IllegalArgumentException.class)
                                                .hasMessageStartingWith("[ERROR] 자동차 이름은 5자 이하만 가능합니다. length:");
+    }
+
+    @DisplayName("자동차는 random 값이 4 이상일 경우 전진한다")
+    @Test
+    void 자동차는_random_값이_4_이상일_경우_전진한다() {
+        try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            // given
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(MOVING_FORWARD);
+
+            // when
+            // then
+            car.play();
+            assertThat(car.getDistance()).isEqualTo(1);
+
+            car.play();
+            assertThat(car.getDistance()).isEqualTo(2);
+        }
+    }
+
+    @DisplayName("자동차는 random 값이 3 이하일 경우 멈춘다")
+    @Test
+    void 자동차는_random_값이_3_이하일_경우_멈춘다() {
+        try (final MockedStatic<Randoms> mock = mockStatic(Randoms.class)) {
+            // given
+            mock.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                .thenReturn(STOP);
+
+            // when
+            // then
+            car.play();
+            assertThat(car.getDistance()).isEqualTo(0);
+
+            car.play();
+            assertThat(car.getDistance()).isEqualTo(0);
+        }
     }
 }
