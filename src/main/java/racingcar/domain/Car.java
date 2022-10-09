c88d80a2c44f4032726bfa5ab15da0073fd35859
my <jvckigh@gmail.com>
2022-10-10 00:41:40 +0900

    feat: 자동차는 random 값이 4 이상일 경우 전진, 3 이하일 경우 멈춘다

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,13 +1,33 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
 
     private static final int MAX_CAR_NAME_LENGTH = 5;
+    private static final int MIN_RANDOM_NO = 0;
+    private static final int MAX_RANDOM_NO = 9;
+    private static final int MOVEABLE_NO = 4;
     private final String name;
+    private int distance;
 
     public Car(String name) {
         checkNameLength(name);
         this.name = name;
+    }
+
+    public void play() {
+        if (canMove()) {
+            distance++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 
     private void checkNameLength(String name) {
@@ -16,7 +36,7 @@ public class Car {
         }
     }
 
-    public String getName() {
-        return name;
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NO, MAX_RANDOM_NO) >= MOVEABLE_NO;
     }
 }
