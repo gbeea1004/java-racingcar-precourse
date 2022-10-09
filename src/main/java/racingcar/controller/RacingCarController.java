5c25da922a790df2f0f4a4c83a9426212db9bea9
my <jvckigh@gmail.com>
2022-10-10 02:07:50 +0900

    feat: 시도할 회수를 입력할 수 있는 기능 추가

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -1,11 +1,13 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
+import racingcar.domain.PlayCount;
 import racingcar.view.InputView;
 
 public class RacingCarController {
 
     public void run() {
         Cars cars = InputView.inputRacingCarNames();
+        PlayCount playCount = InputView.inputPlayCount();
     }
 }
